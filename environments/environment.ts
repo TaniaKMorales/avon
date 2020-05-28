d39ffdfc7d1f9c111fb51499362cb2464ea40bd6
TaniaKMorales <37593222+TaniaKMorales@users.noreply.github.com>
2020-05-28 17:07:40 -0500

    Add files via upload

---
+++ environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAN1yxT-fdW93KRO-LcyffrGaygI-qzdKo",
+    authDomain: "formcontact-fec9b.firebaseapp.com",
+    databaseURL: "https://formcontact-fec9b.firebaseio.com",
+    projectId: "formcontact-fec9b",
+    storageBucket: "formcontact-fec9b.appspot.com",
+    messagingSenderId: "411535365521",
+    appId: "1:411535365521:web:1503f423ffdb09b6e2f630",
+    measurementId: "G-2R3Z5LKV5Y"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
---
+++ message.interface.ts
@@ -0,0 +1,12 @@
+export interface MessageI{
+    name : string;
+    apaterno : string;
+    amaterno : string;
+    direccion : string;
+    telefono : string;
+    celular : string
+    email : string;
+    password : string;
+    conpassword : string;
+
+}
